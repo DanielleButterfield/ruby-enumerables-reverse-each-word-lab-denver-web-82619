41929dd2157274eead7600efeb315f106dbd81eb
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-22 13:42:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,10 @@
 def reverse_each_word(str)
   arr = str.split
-  arr.reverse_each
+  count = 0
+  while arr[count] do
+    arr[count].reverse!
+    count += 1
+  end
   newStr = arr.join
   return newStr
 end
